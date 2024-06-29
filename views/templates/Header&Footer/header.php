76a5c2c1270663fc504da79e018f7cab1de29fcf
TitoManri <manri.carazo@gmail.com>
2024-06-28 19:39:39 -0600

    Header & Footer Prototipo
    Prototipo del navbar con al etiqueta <header> y el footer <footer>

---
+++ views/templates/Header&Footer/header.php
@@ -0,0 +1,55 @@
+<!-- Header -->
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <!-- Navbar Izquierda -->
+    <a class="navbar-brand" href="#"><i class="fa-regular fa-sun fa-2xl"></i></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="navbar-brand" href="#">AYUDITICAS</a>
+        </li>
+      </ul>
+      <form class="d-flex input-group w-auto" role="search">
+      <!-- Navbar Derecha-->
+      <ul class="navbar-nav">
+        <!-- Search Bar -->
+        <!-- Notificaciones -->
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <i class="fa-solid fa-bell fa-2xl"></i>
+          </a>
+        </li>
+        <!-- Publicaciones -->
+        <li class="nav-item dropdown-end">
+            <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="fa-regular fa-pen-to-square fa-2xl"></i>          
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Crear publicación</a></li>
+              <li><a class="dropdown-item" href="#">Crear denuncia</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Crear proyecto</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown-end">
+            <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+            <i class="fa-solid fa-user fa-2xl"></i>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Perfil</a></li>
+              <li><a class="dropdown-item" href="#">Mensajeria</a></li>
+              <li><a class="dropdown-item" href="#">Ver denuncias</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Cerrar sesion</a></li>
+            </ul>
+          </li>
+
+
+        </ul>
+      </form>
+    </div>
+  </div>
+</nav>
