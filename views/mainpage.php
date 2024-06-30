96a1849229806277ec38c2de327a75807dff9c54
TitoManri <manri.carazo@gmail.com>
2024-06-29 21:20:12 -0600

    Exportacion de Archivos para publicaciones y pagina principal
    Creacion de .php y creacion de proptotipo para la pagina principal de la red social

---
+++ views/mainpage.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Principal</title>
+    <!-- Bootstrap & Icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/theme.css">
+    <link rel="stylesheet" href="../assets/css/headerfooter.css">
+    <link rel="stylesheet" href="../assets/css/Red_Social/mainpage.css">
+</head>
+
+<body>
+    <header class="mainHeader">
+        <?php
+        include './templates/Header&Footer/header.php';
+        ?>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <aside class="col-md-2 col-lg-3"style="background-color:red;">
+            </aside>
+            <main class="col d-flex justify-content-center">
+            <div class="col d-flex flex-column align-items-center">
+            <div class="card" style="width: 35rem;">
+                    <img src="https://www.nestleagustoconlavida.com/sites/default/files/2022-04/manos-sembrando.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Tratamos de Ayudar al medio ambiente, tambien puedes hacerlo!</h5>
+                        <p class="card-text">Nuestro planeta, la Tierra, se encuentra en un momento crítico. Las actividades humanas han impactado profundamente el medio ambiente, poniendo en riesgo el delicado equilibrio que sustenta la vida. El cambio climático, la contaminación, la deforestación y la pérdida de biodiversidad son solo algunas de las amenazas que se ciernen sobre nuestro futuro. Es hora de despertar y actuar. Es hora de escuchar el llamado de la Tierra, que nos pide a gritos que cambiemos de rumbo. No podemos seguir ignorando las señales de alarma. El futuro de la Tierra está en nuestras manos.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 35rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Tratamos de Ayudar al medio ambiente, tambien puedes hacerlo!</h5>
+                        <p class="card-text">Nuestro planeta, la Tierra, se encuentra en un momento crítico. Las actividades humanas han impactado profundamente el medio ambiente, poniendo en riesgo el delicado equilibrio que sustenta la vida. El cambio climático, la contaminación, la deforestación y la pérdida de biodiversidad son solo algunas de las amenazas que se ciernen sobre nuestro futuro. Es hora de despertar y actuar. Es hora de escuchar el llamado de la Tierra, que nos pide a gritos que cambiemos de rumbo. No podemos seguir ignorando las señales de alarma. El futuro de la Tierra está en nuestras manos.</p>
+                    </div>
+                </div>
+                <div class="card" style="width: 35rem;">
+                    <img src="https://www.nestleagustoconlavida.com/sites/default/files/2022-04/manos-sembrando.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Tratamos de Ayudar al medio ambiente, tambien puedes hacerlo!</h5>
+                        <p class="card-text">Nuestro planeta, la Tierra, se encuentra en un momento crítico. Las actividades humanas han impactado profundamente el medio ambiente, poniendo en riesgo el delicado equilibrio que sustenta la vida. El cambio climático, la contaminación, la deforestación y la pérdida de biodiversidad son solo algunas de las amenazas que se ciernen sobre nuestro futuro. Es hora de despertar y actuar. Es hora de escuchar el llamado de la Tierra, que nos pide a gritos que cambiemos de rumbo. No podemos seguir ignorando las señales de alarma. El futuro de la Tierra está en nuestras manos.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            </main>
+            <aside class="col-md-2 col-lg-3" style="background-color:red;";>
+            </aside>
+        </div>
+    </div>                        
+
+
+    <footer class="mainfooter">
+        <?php include './templates/Header&Footer/footer.php';
+        ?>
+    </footer>
+</body>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/c723dfe3cd.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="../assets/js/searchBar.js"></script>
+</html>
