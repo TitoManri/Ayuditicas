195f8ab93aa9ce26ac6058a1cec8b5dd624a6a20
anapng <134026852+anapng@users.noreply.github.com>
2024-07-03 14:04:06 -0600

    Creacion de Registro y Perfil
    Creacion de Registro y Perfil, ademas de agregar un header solo para los mismos y un css de igual manera

---
+++ views/registroSesion.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="../assets/css/theme.css">
+  <link rel="stylesheet" href="../assets/css/headerfooter.css">
+  <link rel="stylesheet" href="../assets/css/resgistro.css">
+</head>
+<body>
+<header class="mainHeader">
+        <?php
+            include './templates/Header&Footer/header2.php';
+        ?></header>
+
+<div class="container form-container">
+   <h1 class="mb-3 titulo">Registro de Usuario</h1>
+    <hr>
+        <form class="needs-validation form-container" novalidate="">
+        <div class="row g-3 form-container">
+
+<!--Nombre del usuario-->
+      <div class="col-sm-6">
+        <label for="firstName" class="form-label">Nombre</label>
+        <input type="text" class="form-control escribir border-2" id="firstName" placeholder="" value="" required="">
+          <div class="invalid-feedback">Porfavor ingrese un nombre.</div> <!--Sirven para validar que el campo este completo-->
+      </div>
+
+<!--Apellido del Usuario-->
+      <div class="col-sm-6">
+        <label for="lastName" class="form-label">Apellidos</label>
+        <input type="text" class="form-control escribir" id="lastName" placeholder="" value="" required="">
+          <div class="invalid-feedback">Porfavor ingrese sus apellidos.</div>
+      </div>
+
+<!--Genero Del usuario-->
+      <div class="col-sm-4">
+        <label for="gender" class="form-label">Genero</label>
+        <select class="form-select escribir" aria-label="Default select example" id="gender">
+            <option selected>Eliga su genero</option>
+            <option value="1">Femenino</option>
+            <option value="2">Masculino</option>
+            <option value="3">Prefiero no decidir</option>
+        </select>
+      </div>
+
+<!--Fecha de nacimiento-->
+      <div class="col-sm-8">
+        <label for="fechaNacimiento" class="form-label">Fecha de nacimiento</label>
+        <input type="number" class="form-control escribir" id="fechaNacimiento" placeholder="XX/XX/XXXX" value="" required="">
+          <div class="invalid-feedback">Porfavor ingrese su fecha de nacimiento.</div>
+      </div>
+
+<!--Correo Electronico-->
+      <div class="col-12">
+        <label for="email" class="form-label">Correo electrónico </label>
+        <input type="email" class="form-control escribir" id="email" placeholder="tu@ejemplo.com">
+          <div class="invalid-feedback">Porfavor ingrese un correo electronico valido.</div>
+      </div>
+
+<!--Numero de Cedula del usuario-->
+        <div class="col-sm-8">
+            <label for="numCedula" class="form-label">Numero de cedula</label>
+            <input type="text" class="form-control escribir" id="numCedula" placeholder="" value="" required="">
+              <div class="invalid-feedback">Porfavor ingrese su numero de celuda.</div>
+        </div>
+
+<!--Numero Telefonico-->
+        <div class="col-sm-4">
+            <label for="telefono" class="form-label">Telefono</label>
+            <input type="" class="form-control escribir" id="telefono" placeholder="" value="" required="">
+              <div class="invalid-feedback">Porfavor ingrese su numero de telefono.</div>
+        </div>
+
+<!--Nombre de Usuario-->
+        <div class="col-12">
+        <label for="nomUsuario" class="form-label">Nombre de Usuario</label>
+        <input type="text" class="form-control escribir" id="nomUsuario" placeholder="" required="">
+        </div>
+
+<!--Contraseña-->
+        <div class="col-sm-6">
+            <label for="contraseña" class="form-label">Contraseña</label>
+            <input type="text" class="form-control escribir" id="contraseña" placeholder="" value="" required="">
+              <div class="invalid-feedback">Porfavor ingrese su numero de celuda.</div>
+        </div>
+
+<!--Confirmar Contraseña-->
+        <div class="col-sm-6">
+            <label for="ConfContra" class="form-label">Confirmar Contraseña</label>
+            <input type="text" class="form-control escribir" id="ConfContra" placeholder="" value="" required="">
+              <div class="invalid-feedback">Porfavor ingrese su numero de telefono.</div>
+        </div>
+
+<!--El "Ya teiene una cuenta"-->
+        <div class="col-sm-3">
+        <a class="" href="./inicioSesion.php">¿Ya tienes cuenta? Inicia sesion</a>
+        </div>
+
+        <div class="col-sm-7"></div>
+
+<!--Boton de Registrar-->
+        <div class="col-sm-2">
+        <button class="w-100 btn btn-primary" type="submit" href=""><h4>Registrar</h4></button>
+        </div>
+    </form>
+</div>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/c723dfe3cd.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="../assets/js/searchBar.js"></script>
+</html>
---
+++ views/templates/Header&Footer/header2.php
@@ -0,0 +1,17 @@
+<!-- Header -->
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <!-- Navbar Izquierda -->
+    <a class="navbar-brand" href="#"><i class="fa-regular fa-sun fa-2xl"></i></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="navbar-brand ayuditicaHeader" href="#">AYUDITICAS</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
