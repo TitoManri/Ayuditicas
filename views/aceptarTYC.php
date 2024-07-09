6e65c02d27b9769a77297b664d9ab8c963de0bec
scara14 <144374635+scara14@users.noreply.github.com>
2024-07-09 13:32:04 -0600

    TYC
    Términos y condiciones solo ver

---
+++ views/aceptarTYC.php
@@ -0,0 +1,11 @@
+<div id="FORM" class="container-fluid">
+    <div class="container_label">
+        <h5>Ver términos y condiciones</h5>
+    </div>
+    <div class="container">
+        <form method="post" action="" id="formDenuncia">
+            <h1 class="text-center">Términos y condiciones</h1>
+            <p class="m-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </form>
+    </div>
+</div>
