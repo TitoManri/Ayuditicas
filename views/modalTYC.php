673150ab8382ad4ab109569fd2d90bce24cc80d0
scara14 <144374635+scara14@users.noreply.github.com>
2024-07-10 15:42:43 -0600

    Final tyc
    separar del index

--- views/modalTYC.php
+++ views/modalTYC.php
@@ -1,45 +1,80 @@
-<header class="mainHeader">
-        <?php
-        include './templates/Header&Footer/header.php';
-        ?>
-    </header>
+<!DOCTYPE html>
+<html lang="en">
 
-    <br><br><br><br><br>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Términos y condiciones</title>
+  <!-- Bootstrap & Icons-->
+  <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.2/dist/sweetalert2.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="./assets/css/theme.css">
+  <link rel="stylesheet" href="./assets/css/headerfooter.css">
+</head>
 
-<form method="post" action="" id="registro">
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">Esta es una simulación del formulario de Ana</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
+<body>
+  <header class="mainHeader">
+    <?php
+    include './templates/Header&Footer/header.php';
+    ?>
+  </header>
 
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+  <br><br><br><br><br>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Términos y condiciones</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn text-white" style="background:red" id="rechazarBtn" data-bs-dismiss="modal">Rechazar</button>
-        <button type="button" class="btn text-white" style="background:green" id="aceptarBtn">Aceptar</button>
+  <form method="post" action="" id="registro">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <div id="emailHelp" class="form-text">Esta es una simulación del formulario de Ana</div>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Términos y condiciones</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn text-white" style="background:red" id="rechazarBtn"
+            data-bs-dismiss="modal">Rechazar</button>
+          <button type="button" class="btn text-white" style="background:green" id="aceptarBtn">Aceptar</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-<footer class="mainfooter">
-        <?php include './templates/Header&Footer/footer.php';
-        ?>
-    </footer>
+  <footer class="mainfooter">
+    <?php include './templates/Header&Footer/footer.php';
+    ?>
+  </footer>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.2/dist/sweetalert2.all.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+  integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+  integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/c723dfe3cd.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="./assets/js/searchBar.js"></script>
+<script src="./assets/js/verTYC.js"></script>
+<script src="./assets/js/modalTYC.js"></script>
+
+</html>
--- views/verTYC.php
+++ views/verTYC.php
@@ -1,26 +1,60 @@
-<header class="mainHeader">
-    <?php
-    include './templates/Header&Footer/header.php';
-    ?>
-</header>
+<!DOCTYPE html>
+<html lang="en">
 
-<div id="FORM" class="container-fluid">
-    <div class="container_label">
-        <h5>Ver términos y condiciones</h5>
-    </div>
-    <div class="container">
-        <form method="post" action="" id="formDenuncia">
-            <h1 class="text-center">Términos y condiciones</h1>
-            <p class="m-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                qui officia deserunt mollit anim id est laborum.</p>
-        </form>
-    </div>
-</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Términos y condiciones</title>
+    <!-- Bootstrap & Icons-->
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.2/dist/sweetalert2.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/theme.css">
+    <link rel="stylesheet" href="./assets/css/headerfooter.css">
+    <link rel="stylesheet" href="./assets/css/verTYC.css">
+</head>
 
-<footer class="mainfooter">
-    <?php include './templates/Header&Footer/footer.php';
-    ?>
-</footer>
+<body>
+    <header class="mainHeader">
+        <?php
+        include './templates/Header&Footer/header.php';
+        ?>
+    </header>
+
+    <div id="FORM" class="container-fluid">
+        <div class="container_label">
+            <h5>Ver términos y condiciones</h5>
+        </div>
+        <div class="container">
+            <form method="post" action="" id="formDenuncia">
+                <h1 class="text-center">Términos y condiciones</h1>
+                <p class="m-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi
+                    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa
+                    qui officia deserunt mollit anim id est laborum.</p>
+            </form>
+        </div>
+    </div>
+
+    <footer class="mainfooter">
+        <?php include './templates/Header&Footer/footer.php';
+        ?>
+    </footer>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.2/dist/sweetalert2.all.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+    crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/c723dfe3cd.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="./assets/js/searchBar.js"></script>
+<script src="./assets/js/verTYC.js"></script>
+
+</html>
