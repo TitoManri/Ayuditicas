ef061166d383a0602ca4516d224712ef7fde06ad
JorgeABCT <jabarcacoto05@gmail.com>
2024-07-10 01:38:33 -0600

    Cambios a Etiquetas

--- views/VerEtiquetas.php
+++ views/VerEtiquetas.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina Principal</title>
     <!-- Bootstrap & Icons-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/theme.css">
     <link rel="stylesheet" href="./assets/css/headerfooter.css">
@@ -27,74 +28,55 @@
     <br>
 
     <section id="BusquedaEtiqueta" class="container">
-        <div style="width: 30%;">
-            <!--Preguntar cual de las dos les parece
+        <form action="" method="GET">
+            <div style="width: 30%;">
+                <!--Preguntar cual de las dos les parece
             <div class="input-group mb-3">
                 <span class="input-group-text" id="inputEtiqueta"><i class="fa fa-search"></i></span>
                 <input type="text" class="form-control" placeholder="Buscar por Nombre" id="inputEtiqueta" aria-describedby="inputEtiqueta">
             </div>
         -->
-            <label for="SelectEtiqueta"><i class="fa fa-search"></i> Busqueda por nombre</label>
-            <select name="SelectEtiqueta" id="SelectEtiqueta" multiple>
-                <option value="1">Basura</option>
-                <option value="2">Arboles</option>
-                <option value="3">Educacion</option>
-            </select>
+                <label for="SelectEtiqueta"><i class="fa fa-search"></i> Busqueda por nombre</label>
+                <div class="d-flex justify-content-center">
+                    <select name="SelectEtiqueta" id="SelectEtiqueta" multiple required>
+                        <?php
+                        $opciones = ['Basura', 'Arboles', 'Educacion'];
+
+                        for ($i = 0; $i < count($opciones); $i++) {
+                            echo '<option value="', $opciones[$i], '">', $opciones[$i], '</option>';
+                        }
+                        ?>
+                    </select>
+                    <button type="submit" class="btn btn-light"><i class="bi bi-search"></i></button>
+                </div>
+        </form>
         </div>
     </section>
     <br>
     <h2 class="text-center">Etiquetas Populares</h2>
     <br>
     <section id="EtiquetasPopulares" class="container">
-        <div class="row pb-5">
-            <div class="col-1"></div>
-            <div class="EtiquetasRecomendados col-3 centro">
-                <a href="#">
-                    <h3>Arboles</h3>
-                    <h4>300 publicaciones</h4>
-                </a>
-            </div>
-            <div class="col-1"></div>
-            <div class="EtiquetasRecomendados col-3 centro">
-                <a href="#">
-                    <h3>Arboles</h3>
-                    <h4>300 publicaciones</h4>
-                </a>
-            </div>
-            <div class="col-1"></div>
-            <div class="EtiquetasRecomendados col-3 centro">
-                <a href="#">
-                    <h3>Arboles</h3>
-                    <h4>300 publicaciones</h4>
-                </a>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-1"></div>
-            <div class="EtiquetasRecomendados col-3 centro">
-                <a href="#">
-                    <h3>Arboles</h3>
-                    <h4>300 publicaciones</h4>
-                </a>
-            </div>
-            <div class="col-1"></div>
-            <div class="EtiquetasRecomendados col-3 centro">
-                <a href="#">
-                    <h3>Arboles</h3>
-                    <h4>300 publicaciones</h4>
-                </a>
-            </div>
-            <div class="col-1"></div>
-            <div class="EtiquetasRecomendados col-3 centro">
-                <a href="#">
-                    <h3>Arboles</h3>
-                    <h4>300 publicaciones</h4>
-                </a>
-            </div>
-        </div>
+        <?php
+        $opcionesEtiquetas = [['Arboles', 400], ['Arboles', 200], ['Arboles', 100], ['Arboles', 50]];
+        for ($i = 0; $i < count($opcionesEtiquetas); $i++) {
+            if ($i % 3 == 0 || $i == 0) {
+                echo '<div class="row pb-5">';
+            }
+            echo '<div class="col-1"></div><a class="EtiquetasRecomendados col-3 text-center" href="#">
+                <br>
+                <br>
+                <h3>', $opcionesEtiquetas[$i][0], '</h3>
+                <br>
+                <h4>', $opcionesEtiquetas[$i][1], ' publicaciones</h4>
+            </a>';
+            if(($i+1) % 3 == 0 || $i == count($opcionesEtiquetas)) {
+                echo '</div>';
+            }
+        }
+        ?>
     </section>
 
-    <br><br><br><br><br><br><br><br><br><br>
+    <br><br>
 
     <footer class="mainfooter">
         <?php include './templates/Header&Footer/footer.php';
