7b56491434a8fa0662526b426739a807a1b2e2d9
scara14 <144374635+scara14@users.noreply.github.com>
2024-07-15 10:51:09 -0600

    Arreglos finales
    cambios en denuncias arreglo del contenedor y centrado, comentarios js

--- views/confirmarDenuncia.php
+++ views/confirmarDenuncia.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.2/dist/sweetalert2.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="./assets/css/confDenuncia.css">
+    <link rel="stylesheet" href="./assets/css/denuncias.css">
     <link rel="stylesheet" href="./assets/css/headerfooter.css">
     <link rel="stylesheet" href="./assets/css/theme.css">
 </head>
@@ -37,8 +37,8 @@
                     <label>
                         <h3>Imágen</h3>
                     </label>
-                    <div class="imagenPreviewContainer">
-                        <img id="imagenPreview" src="" alt="Vista previa de la imagen">
+                    <div class="imgDenunciaContainer">
+                        <img id="imgDenuncia" src="" alt="Vista previa de la imagen">
                     </div>
                 </div>
             </div>
