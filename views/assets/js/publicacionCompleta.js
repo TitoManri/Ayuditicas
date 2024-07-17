a85f44810a2d2cbae16a3ae9c1c3a089e9421b75
TitoManri <manri.carazo@gmail.com>
2024-07-17 14:12:39 -0600

    Cambios pagina principal
    .

--- views/assets/js/publicacionCompleta.js
+++ views/assets/js/publicacionCompleta.js
@@ -1,35 +1,35 @@
-function addNewComment() {
+function agregarNuevoComentario() {
     //Comentario del textarea
-    const newCommentText = document.getElementById('nuevo-comentario-text').value;
+    const nuevoComentario = document.getElementById('nuevo-comentario-text').value;
     //Evitar que se pongan espacios en blanco
-    if (newCommentText.trim() !== '') {
+    if (nuevoComentario.trim() !== '') {
         //Lista no ordenada de los comentarios anteriores
-        const commentsList = document.getElementById('commentarios-lista');
+        const listaComentarios = document.getElementById('commentarios-lista');
         //Se usa para ver si hay comentarios o no, si no muestra el mensaje de No hay comentarios aun.
-        const noCommentsMessage = document.getElementById('no-commentarios');
+        const comentarioNoMensajes = document.getElementById('no-commentarios');
         //Crea la etiqueta <li> para la lista
-        const newCommentItem = document.createElement('li');
+        const nuevoItemComentario = document.createElement('li');
         //Agregar el css de comentario
-        newCommentItem.classList.add('comentario', 'mt-2');
+        nuevoItemComentario.classList.add('comentario', 'mt-2');
         //Agrega el comentario escrito por el usuario
-        newCommentItem.textContent = newCommentText;
+        nuevoItemComentario.textContent = nuevoComentario;
 
         //Remplaza los saltos de linea del usuario por <br> para simular el salto
-        newCommentItem.innerHTML = newCommentItem.innerHTML.replace(/\n/g, '<br>');
+        nuevoItemComentario.innerHTML = nuevoItemComentario.innerHTML.replace(/\n/g, '<br>');
 
         //Agrega el comentario al final de la lista
-        commentsList.appendChild(newCommentItem);
+        listaComentarios.appendChild(nuevoItemComentario);
 
         //Limpia el textarea despues de agregar el comentario
         document.getElementById('nuevo-comentario-text').value = '';
 
         //Oculta el mensaje de 'No hay comentarios aun' si si hay comentarios
-        if (noCommentsMessage) {
-            noCommentsMessage.style.display = 'none';
+        if (comentarioNoMensajes) {
+            comentarioNoMensajes.style.display = 'none';
         }
 
         //Hace el "scroll" hasta el fondo para ver el comentario nuevp
-        commentsList.scrollTop = commentsList.scrollHeight;
+        listaComentarios.scrollTop = listaComentarios.scrollHeight;
     }
 }
 
@@ -37,6 +37,6 @@ function addNewComment() {
 document.getElementById('nuevo-comentario-text').addEventListener('keydown', function (event) {
     if (event.key === 'Enter' && !event.shiftKey) {  
         event.preventDefault();
-        addNewComment();
+        agregarNuevoComentario();
     }
 });
--- views/templates/Publicaciones/publicacionCompleta.php
+++ views/templates/Publicaciones/publicacionCompleta.php
@@ -1,33 +1,32 @@
 <div class="container contenedor-publicacion">
     <div class="row">
         <div class="col-md-8">
-            <!-- Contenido Principal de la Publicación -->
             <div class="encabezado-publicacion">
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="d-flex align-items-center">
-                        <i class="fa-solid fa-user fa-lg"></i>
-                        <h5 class="mb-0 ml-3">&nbsp;Manrique Carazo</h5>
+                        <i class="fa-solid fa-user fa-lg" style="color:black;"></i>
+                        <h5 class="mb-0 ml-3 text-dark">&nbsp; Manrique Carazo</h5>
                     </div>
                     <div class="likes d-flex align-items-center mt-2">
                         <i class="fa-regular fa-heart fa-lg heart-icon"></i>
-                        <span class="ml-2">&nbsp;10 likes</span>
+                        <span class="ml-2 text-dark">&nbsp; 10 likes</span>
                     </div>
                 </div>
             </div>
             <div class="contenido-publicacion">
                 <img src="https://www.nestleagustoconlavida.com/sites/default/files/2022-04/manos-sembrando.jpg"
                     class="imagen-publicacion" alt="publicacionImagen">
-                <h5 class="mt-3">Tratamos de Ayudar al medio ambiente, tambien puedes hacerlo! </h5>
+                <h5 class="mt-3 text-dark">Tratamos de Ayudar al medio ambiente, tambien puedes hacerlo! </h5>
             </div>
         </div>
         <div class="col-md-4 caja-comentarios">
             <div class="encabezado-publicacion-comentarios">
                 <div class="d-flex justify-content-between align-items-center">
-                    <h5 class="">Comentarios</h5>
+                    <h5 class="text-dark">Comentarios</h5>
                     <div class="nav-item dropdown">
                         <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" data-bs-display="static"
                             aria-expanded="false">
-                            <i class="fa-solid fa-ellipsis-vertical fa-lg"></i>
+                            <i class="fa-solid fa-ellipsis-vertical fa-lg" style="color:black;"></i>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-lg-end dropdown-report">
                             <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#reportar" href="#"><i
