a13aec4c73bdbee6b1448469f9b3bf0a0c8bc6ee
anapng <134026852+anapng@users.noreply.github.com>
2024-07-23 15:07:13 -0600

    Actualizacion del inicio de sesion y registro de sesion

--- views/registroSesion.php
+++ views/registroSesion.php
@@ -23,22 +23,29 @@
 
 <!--Nombre del usuario-->
       <div class="col-sm-6">
-        <label for="firstName" class="form-label">Nombre</label>
-        <input type="text" class="form-control escribir" id="firstName" placeholder="" value="" required>
+        <label for="nombre" class="form-label">Nombre</label>
+        <input type="text" class="form-control escribir" id="nombre" placeholder="" value="" required name="nombre">
           <div class="invalid-feedback">Porfavor ingrese un nombre.</div> <!--Sirven para validar que el campo este completo-->
       </div>
 
 <!--Apellido del Usuario-->
-      <div class="col-sm-6">
-        <label for="lastName" class="form-label">Apellidos</label>
-        <input type="text" class="form-control escribir" id="lastName" placeholder="" value="" required="">
+      <div class="col-sm-3">
+        <label for="primer_apellido" class="form-label">Primer Apellido</label>
+        <input type="text" class="form-control escribir" id="primer_apellido" placeholder="" value="" required="" name="primer_apellido">
+          <div class="invalid-feedback">Porfavor ingrese sus apellidos.</div>
+      </div>
+
+<!--Apellido del Usuario-->
+<div class="col-sm-3">
+        <label for="segundo_apellido" class="form-label">Segundo Apellido</label>
+        <input type="text" class="form-control escribir" id="segundo_apellido" placeholder="" value="" required="" name="segundo_apellidop">
           <div class="invalid-feedback">Porfavor ingrese sus apellidos.</div>
       </div>
 
 <!--Genero Del usuario-->
       <div class="col-sm-4">
-        <label for="gender" class="form-label">Genero</label>
-        <select class="form-select escribir" aria-label="Default select example" id="gender">
+        <label for="genero" class="form-label">Genero</label>
+        <select class="form-select escribir" aria-label="Default select example" id="genero" name="genero">
             <option selected>Eliga su genero</option>
             <option value="1">Femenino</option>
             <option value="2">Masculino</option>
@@ -49,48 +56,48 @@
 <!--Fecha de nacimiento-->
       <div class="col-sm-8">
         <label for="fechaNacimiento" class="form-label">Fecha de nacimiento</label>
-        <input type="date" class="form-control escribir" id="fechaNacimiento" placeholder="XX/XX/XXXX" value="" required="">
+        <input type="date" class="form-control escribir" id="fechaNacimiento" placeholder="XX/XX/XXXX" value="" required="" name="fechaNacimiento">
           <div class="invalid-feedback">Porfavor ingrese su fecha de nacimiento.</div>
       </div>
 
 <!--Correo Electronico-->
       <div class="col-12">
-        <label for="email" class="form-label">Correo electrónico </label>
-        <input type="email" class="form-control escribir" id="email" placeholder="tu@ejemplo.com">
+        <label for="correo" class="form-label">Correo electrónico </label>
+        <input type="email" class="form-control escribir" id="correo" placeholder="tu@ejemplo.com" name="correo">
           <div class="invalid-feedback">Porfavor ingrese un correo electronico valido.</div>
       </div>
 
 <!--Numero de Cedula del usuario-->
         <div class="col-sm-8">
-            <label for="numCedula" class="form-label">Numero de cedula</label>
-            <input type="number" class="form-control escribir" id="numCedula" placeholder="" value="" required="">
+            <label for="cedula" class="form-label">Numero de cedula</label>
+            <input type="number" class="form-control escribir" id="cedula" placeholder="" value="" required="" name="cedula">
               <div class="invalid-feedback">Porfavor ingrese su numero de celuda.</div>
         </div>
 
 <!--Numero Telefonico-->
         <div class="col-sm-4">
             <label for="telefono" class="form-label">Telefono</label>
-            <input type="number" class="form-control escribir" id="telefono" placeholder="" value="" required="">
+            <input type="number" class="form-control escribir" id="telefono" placeholder="" value="" required="" name="telefono">
               <div class="invalid-feedback">Porfavor ingrese su numero de telefono.</div>
         </div>
 
 <!--Nombre de Usuario-->
         <div class="col-12">
-        <label for="nomUsuario" class="form-label">Nombre de Usuario</label>
-        <input type="text" class="form-control escribir" id="nomUsuario" placeholder="" required="">
+        <label for="nombre_usuario" class="form-label">Nombre de Usuario</label>
+        <input type="text" class="form-control escribir" id="nombre_usuario" placeholder="" required="" name="nombre_usuario">
         </div>
 
 <!--Contraseña-->
         <div class="col-sm-6">
-            <label for="contraseña" class="form-label">Contraseña</label>
-            <input type="password" class="form-control escribir" id="contraseña" placeholder="" value="" required="">
+            <label for="contrasenia" class="form-label">Contraseña</label>
+            <input type="password" class="form-control escribir" id="contrasenia" placeholder="" value="" required="" name="contrasenia">
               <div class="invalid-feedback">Porfavor ingrese su numero de celuda.</div>
         </div>
 
 <!--Confirmar Contraseña-->
         <div class="col-sm-6">
-            <label for="ConfContra" class="form-label">Confirmar Contraseña</label>
-            <input type="password" class="form-control escribir" id="ConfContra" placeholder="" value="" required="">
+            <label for="confirmar_contrasenia" class="form-label">Confirmar Contraseña</label>
+            <input type="password" class="form-control escribir" id="confirmar_contrasenia" placeholder="" value="" required="" name="confirmar_contrasenia">
               <div class="invalid-feedback">Porfavor ingrese su numero de telefono.</div>
         </div>
 
@@ -104,10 +111,18 @@
 <!--Boton de Registrar-->
         <div class="col-sm-3">
         <!--Checkbox para el Aceptar Terminos y Condiciones-->
-        <div class="checkbox mb-3">
-          <label> <input type="checkbox" value="remember-me">&nbsp;Aceptar Terminos y Condiciones</label>
+        <div class="checkbox-button mb-3">
+          <input type="checkbox" id="TYCCheck">
+          <label for="TYCCheck" class="" id="TYBbtn" data-bs-toggle="modal" data-bs-target="#exampleModal" name="TYBbtn">
+            Aceptar Términos y Condiciones
+          </label>
         </div>
-        <button class="w-100 btn btn-success" type="submit" href=""><h4>Registrar</h4></button>
+        <?php
+          include './templates/registro/MRegistroSesion.php'
+        ?>
+
+        <!--Boton de registrarse-->
+        <button class="w-100 btn-success" type="submit" href=""><h4>Registrar</h4></button>
         </div>
         <br><br><br><br><br><br><br><br>
     </form>
@@ -121,5 +136,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/c723dfe3cd.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="../assets/js/searchBar.js"></script>
+    <script src="../assets/js/MTYCRS.js"></script>
 </html>
