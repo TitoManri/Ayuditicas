19faf5775e38c1cc47bf8c3458dfcb509e75f088
anapng <134026852+anapng@users.noreply.github.com>
2024-07-24 14:57:42 -0600

    Cambios en el css de perfil
    Cambios realizados en el apartado del css del perfil y remocion de la clase css de campanias

--- views/perfil.php
+++ views/perfil.php
@@ -9,8 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/theme.css">
     <link rel="stylesheet" href="./assets/css/headerfooter.css">
-    <link rel="stylesheet" href="../views/assets/css/perfil.css">
-    <link rel="stylesheet" href="./assets/css/campanias.css">
+    <link rel="stylesheet" href="../views/assets/css/perfil.css"> 
 </head>
 <body>
     <header class="mainHeader">
