444fb9b9bf8012f9e728817cbd98ba6b99e30b98
TitoManri <manri.carazo@gmail.com>
2024-08-20 16:06:57 -0600

    Commit con integracion de Comentarios
    ..

--- views/assets/js/publicacion.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() => {
-    $(document.body).on('click', '.card[data-clickable=true]', (e) => {
-      var href = $(e.currentTarget).data('href');
-      window.location = href;
-    });
-  });
